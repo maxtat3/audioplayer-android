ffb98cb0c448c2b690d227a763aa7a4ce7e4f1bf
Max <multi3volume@gmail.com>
2016-05-20 16:19:18 +0300

    In playlist when clicked from item - start playing audio track.

--- app/src/main/java/edu/sintez/audioplayer/app/activity/MainActivity.java
+++ app/src/main/java/edu/sintez/audioplayer/app/activity/MainActivity.java
@@ -408,6 +408,7 @@ public class MainActivity extends Activity implements
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 		selTrackPos = position;
+		playTrack();
 	}
 
 	@Override
