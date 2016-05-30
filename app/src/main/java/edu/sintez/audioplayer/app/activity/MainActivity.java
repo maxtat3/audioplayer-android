b61979ffcaa20ef3684db088a4655a961525cf00
Max <multi3volume@gmail.com>
2016-05-30 19:11:29 +0300

    Removed web icons.

--- app/src/main/java/edu/sintez/audioplayer/app/activity/MainActivity.java
+++ app/src/main/java/edu/sintez/audioplayer/app/activity/MainActivity.java
@@ -209,7 +209,7 @@ public class MainActivity extends Activity implements
 		sBarProgress.setOnSeekBarChangeListener(this);
 
 		registerReceivers();
-//		createSearchInActionBar();
+		createSearchInActionBar();
 		receivedAudioFromIntent();
 	}
 
